8cb36cfb94d7c7c126a87d97d5ef458db71e5f5c
Olumide Micheal <olumidemicheal7@gmail.com>
2023-09-10 09:37:00 +0100

    added custom style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,37 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      normal: '20px',
+      md: '24px',
+      lg: '32px',
+      xl: '40px',
+      xxl: '48px',
+      xxxl: '64px'
+    },
+    colors: {
+      white: "#FFFFFF",
+      black: '#1E1E1E',
+      blue: "#2F7AEA",
+      green: "#5ECC8B",
+      gray: "#8B8D8F",
+      lightGray: "#F8F9FB",
+      darkGray: "#53585B",
+      red: "#E60000",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        'sm': '200px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
     },
   },
